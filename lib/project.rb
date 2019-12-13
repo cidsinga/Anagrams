ecd71999821063f4e43239b5cfbeeff6b6634f43
cidsinga <56556816+cidsinga@users.noreply.github.com>
2019-12-13 11:04:03 -0800

    attempts at meaningful 2nd test, testing if it's an anagram

--- lib/project.rb
+++ lib/project.rb
@@ -16,13 +16,25 @@ class Anagram
     elsif @input1.include? "e"
     elsif @input1.include? "i"
     elsif @input1.include? "o"
-
-
+    elsif @input1.include? "u"
+    elsif @input1.include? "y"
     # @input1.scan(/[aeiou]/) && @input2.scan(/[aeiou]/) == true
     return true
   else
     return "please enter words"
   end
+
+  def anagram_check(input1, input2)
+    if @input1.chars.sort.join == @input2.chars.sort.join
+      return true
+    else
+      return "not and anagram"
+
+
+  end
+
 end
 end
+end
+
     # anagram = Anagram.new("ruby", "bury")
--- spec/project_spec.rb
+++ spec/project_spec.rb
@@ -2,10 +2,15 @@ require('rspec')
 require('project')
 
 
-describe('Anagram#vowel_check') do
+describe('Anagram') do
   it("should return True if word contains a vowel") do
     anagram = Anagram.new("rby", "bry")
     expect(anagram.vowel_check("rby", "bry")).to(eq(true))
+  end
+
+  it("should return it's an anagram if true") do
+    anagram = Anagram.new("ruby", "bury")
+    expect(anagram.anagram_check("ruby", "bury")).to(eq(true))
   end
 
 end
