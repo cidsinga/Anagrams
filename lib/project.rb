d4064d2be07bc5e3d4910859c7ffc70125d94d31
cidsinga <56556816+cidsinga@users.noreply.github.com>
2019-12-13 10:03:03 -0800

    add first meaningful test and passing

--- lib/project.rb
+++ lib/project.rb
@@ -1,8 +1,8 @@
 # !/usr/bin/ruby
-psudo code - take 2 inputs
-ensure that they are words by if they have vowels
-compare char length - if != they're not anagrams--- unnecesary
-if so sort letters and compare to uppercase
+# psudo code - take 2 inputs
+# ensure that they are words by if they have vowels
+# compare char length - if != they're not anagrams--- unnecesary
+# if so sort letters and compare to uppercase
 
 class Anagram
   def initialize(input1, input2)
@@ -10,9 +10,14 @@ class Anagram
     @input2 = input2
   end
 
-  def vowel_check()
-    str =~ /[aeiouy]/
+  def vowel_check(input1, input2)
+    if
+    @input1.scan(/[aeiou]/) && @input2.scan(/[aeiou]/)
+    return true
+  else
+    return "please enter words"
+
   end
-
-
 end
+end
+    anagram = Anagram.new("ruby", "bury")
--- spec/project_spec.rb
+++ spec/project_spec.rb
@@ -3,9 +3,9 @@ require('project')
 
 
 describe('Anagram#vowel_check') do
-  it("should return True is word contains a vowel") do
-    anagram = Anagram.new(ruby, bury)
-    expect(anagram.vowel_check()).to(eq(true))
+  it("should return True if word contains a vowel") do
+    anagram = Anagram.new("ruby", "bury")
+    expect(anagram.vowel_check("ruby", "bury")).to(eq(true))
   end
 
 end
