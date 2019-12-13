0eb20cae51892b36084f485d13b27fdf5807f508
cidsinga <56556816+cidsinga@users.noreply.github.com>
2019-12-13 09:14:10 -0800

    add psudo code and initial class and test framework

--- lib/project.rb
+++ lib/project.rb
@@ -1,50 +1,18 @@
 # !/usr/bin/ruby
-class CoinCounter
-  def initialize(input)
-    @input = input
-    @currentvalue = 0
-    @quarters = 0
-    @dimes = 0
-    @nickels = 0
-    @pennies = 0
-  end
-  def currentvalue(currentvalue)
-    @currentvalue = currentvalue
-  end
-  def quarters(quarters)
-    @quarters = quarters
-  end
-  def dimes(dimes)
-    @dimes = dimes
-  end
-  def nickels(nickels)
-    @nickels = nickels
-  end
-  def pennies(pennies)
-    @pennies = pennies
+psudo code - take 2 inputs
+ensure that they are words by if they have vowels
+compare char length - if != they're not anagrams--- unnecesary
+if so sort letters and compare to uppercase
+
+class Anagram
+  def initialize(input1, input2)
+    @input1 = input1
+    @input2 = input2
   end
 
-  def quarter_counter()
-    self.quarters((@input / 0.25).floor)
-# *or* @quarters = (@input / 0.25).floor
-    @@currentvalue = @input % 0.25
+  def vowel_check()
+    str =~ /[aeiouy]/
   end
-  def dime_counter()
-    self.dimes((@@currentvalue / 0.10).floor)
-# *or* @dime = (@currentvalue / 0.1).floor
-    @@currentvalue = (@@currentvalue % 0.10).round(2)
-  end
-  def nickel_counter()
-    self.nickels((@@currentvalue / 0.05).floor)
-    puts @nickels
-# *or* @dime = (@currentvalue / 0.1).floor
-    @@currentvalue = (@@currentvalue % 0.05).round(3)
-  end
-  def penny_counter()
-    self.pennies((@@currentvalue / 0.01).floor)
-    puts @pennies
-# *or* @dime = (@currentvalue / 0.1).floor
-    @@currentvalue = (@@currentvalue % 0.01).round(3)
-  end
-  puts "you have " + @quarters + "quarters, " + @dimes + "dimes, " + @nickels "nickels, and " @pennies + "pennies!"
+
+
 end
--- spec/project_spec.rb
+++ spec/project_spec.rb
@@ -2,31 +2,10 @@ require('rspec')
 require('project')
 
 
-describe('CoinCounter#quarter_counter') do
-  it("should return the remainder of input / .25") do
-    coincounter = CoinCounter.new(0.99)
-    expect(coincounter.quarter_counter()).to(eq(0.24))
+describe('Anagram#vowel_check') do
+  it("should return True is word contains a vowel") do
+    anagram = Anagram.new(ruby, bury)
+    expect(anagram.vowel_check()).to(eq(true))
   end
-  it("should return the remainder of currentvalue / .10") do
-    coincounter = CoinCounter.new(0.99)
-    expect(coincounter.dime_counter()).to(eq(0.04))
-  end
-  it("should return the remainder of currentvalue / .05") do
-    coincounter = CoinCounter.new(0.99)
-    expect(coincounter.nickel_counter()).to(eq(0.04))
-  end
-  it("should return the remainder of currentvalue / .01") do
-    coincounter = CoinCounter.new(0.99)
-    expect(coincounter.penny_counter()).to(eq(0.00))
-  end
-  # it("should return the number of quarters") do
-  #   coincounter = CoinCounter.new(0.99)
-  #   expect(coincounter.quarter_counter(0.99)).to(eq(3))
-  # end
 
-# it("returns a Player 1 win total as 1") do
-#   game = Game.new("rock", "scissors")
-#   game.game_win()
-#   expect(game.player_one_wins()).to(eq(1))
-# end
 end
